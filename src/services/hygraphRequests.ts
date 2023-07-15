978e4d22db0f674e8985422bc5c999910c80c47a
Fred Brossard <fred.b.devweb@gmail.com>
2023-07-15 19:07:11 +0200

    Feat : hero, presentation section, Practices banner

--- src/services/hygraphRequests.ts
+++ src/services/hygraphRequests.ts
@@ -1,23 +1,62 @@
 import { request, gql } from "graphql-request";
+import type { HomePages, Practice } from "@/types/types.d.ts";
 const graphqlAPI: string = import.meta.env.GRAPHCMS_ENDPOINT!;
-
+type ResultHomePages = {
+  homePages: HomePages;
+};
+type ResultPractices = Practice[];
 
 export const getHomePageData = async () => {
-    const query = gql`
-      query getHomePageData {
-        homePages(orderBy: publishedAt_ASC, last: 1) {
-          hero {
-            title
-            subtitle
-            heroImage {
-              height
-              url
-              width
-            }
+  const query = gql`
+    query getHomePageData {
+      homePages(orderBy: publishedAt_ASC, last: 1) {
+        hero {
+          title
+          subtitle
+          image {
+            height
+            width
+            url
           }
         }
+        titlePresentation
+        contentPresentation
+        practicesSection {
+          image {
+            height
+            url
+            width
+          }
+          title
+          subtitle
+        }
+        practicesPresentation
       }
-    `;
-    const result = await request(graphqlAPI, query);
-    return result;
-}
+    }
+  `;
+  const result: ResultHomePages = await request(graphqlAPI, query);
+  console.log(result)
+  return result.homePages[0];
+};
+
+export const getPractices = async () => {
+  const query = gql`
+    query GetPractices {
+      practices(orderBy: order_ASC) {
+        order
+        image {
+          height
+          url
+          width
+        }
+        slug
+        title
+        content
+        shortContent
+      }
+    }
+  `;
+  const result: ResultPractices = await request(graphqlAPI, query);
+  console.log(result);
+  return result;
+};
--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,17 +1,38 @@
-export type HeroImageData= {
-    width:number
-    height: number
-    url: string
+import { HeroData } from './types.d';
+export type Image = {
+  height:number
+          url:string
+          width: number
 }
+
+export type HeroImageData= Image
+    
 
 export type HeroData = {
   title: string;
   subtitle: string;
-  heroImage: {
-    width: number;
-    height: number;
-    url: string;
-  };
+  image: Image
 };
 
-export type HomePageData = HeroData;
+export type HomePageData = {
+  hero: HeroData;
+  titlePresentation: string;
+  contentPresentation: string;
+  practicesSection: {
+    image: Image;
+    title: string;
+    subtitle: string;
+  };
+  practicesPresentation:string;
+};
+
+export type HomePages = HomePageData[]
+
+export type Practice = {
+  order:number
+  image:Image[]
+  slug:string
+  title:string
+  shortContent:string
+  content:string
+};
